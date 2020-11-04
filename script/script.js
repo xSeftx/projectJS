6da21c6e92758e094e3c9e4d31c0f733f504d47c
Alexander <57403744+xSeftx@users.noreply.github.com>
2020-11-04 14:50:34 +0400

    Добавил 19 задание

--- script/script.js
+++ script/script.js
@@ -1,52 +1,110 @@
 window.addEventListener('DOMContentLoaded', function(){
     'use strict';
 
-// Timer
-function countTimer(deadLine){
-    let timerHours = document.querySelector('#timer-hours'),
-        timerMinutes = document.querySelector('#timer-minutes'),
-        timerSeconds = document.querySelector('#timer-seconds');
+    
+    
 
-    function getTimeRemaining(){
-        let dateStop = new Date(deadLine).getTime(),
-            dateNow = new Date().getTime(),
-            timeRemaining = (dateStop - dateNow) / 1000,
-            seconds = Math.floor(timeRemaining % 60),
-            minutes = Math.floor((timeRemaining / 60) % 60),
-            hours = Math.floor(timeRemaining / 60 / 60);
-            return {timeRemaining, hours, minutes, seconds};
-    }
 
-    function updateClock(){
-        let timer = getTimeRemaining(); 
-        let getZero = function(nam){
-            if (nam > 0 && nam < 10) { 
-                return '0' + nam;
-            } else {
-                return nam;
-            }
-            };       
-        timerHours.textContent = getZero(timer.hours);
-        timerMinutes.textContent = getZero(timer.minutes);
-        timerSeconds.textContent = getZero(timer.seconds);
 
-        if(timer.timeRemaining > 0){
+    // Timer
+    function countTimer(deadLine){
+        const timerHours = document.querySelector('#timer-hours'),
+            timerMinutes = document.querySelector('#timer-minutes'),
+            timerSeconds = document.querySelector('#timer-seconds');
 
-            let idInterval = setInterval(updateClock, 1000);
-        } else {
-            timerHours.textContent = '00';
-            timerMinutes.textContent = '00';
-            timerSeconds.textContent = '00';
-            // clearInterval(idInterval);
+        function getTimeRemaining(){
+            const dateStop = new Date(deadLine).getTime(),
+                dateNow = new Date().getTime(),
+                timeRemaining = (dateStop - dateNow) / 1000,
+                seconds = Math.floor(timeRemaining % 60),
+                minutes = Math.floor((timeRemaining / 60) % 60),
+                hours = Math.floor(timeRemaining / 60 / 60);
+                return {timeRemaining, hours, minutes, seconds};
         }
-        
-        
+
+        function updateClock(){
+            let timer = getTimeRemaining(); 
+            let getZero = function(nam){
+                if (nam >= 0 && nam < 10) { 
+                    return '0' + nam;
+                } else {
+                    return nam;
+                }
+                };       
+            timerHours.textContent = getZero(timer.hours);
+            timerMinutes.textContent = getZero(timer.minutes);
+            timerSeconds.textContent = getZero(timer.seconds);
+
+            if(timer.timeRemaining > 0){
+
+                let idInterval = setInterval(updateClock, 1000);
+            } else {
+                timerHours.textContent = '00';
+                timerMinutes.textContent = '00';
+                timerSeconds.textContent = '00';
+                // clearInterval(idInterval);
+            }
+            
+            
+        }
+
+        updateClock();
     }
+    countTimer('6 november 2020');
 
-    updateClock();
-}
+    // Menu
+    const toggleMenu = () =>{
+        const btnMenu = document.querySelector('.menu'),
+            menu = document.querySelector('menu'),
+            closeBtn = document.querySelector('.close-btn'),
+            menuItems = menu.querySelectorAll('ul>li');
 
-countTimer('3 november 2020');
+        const handlerMenu = () => {
+            menu.classList.toggle('active-menu');
+        };          
+        btnMenu.addEventListener('click', handlerMenu);
+        closeBtn.addEventListener('click', handlerMenu);
+        menuItems.forEach((elem) => elem.addEventListener('click', handlerMenu));
+
+    };
+
+    toggleMenu();
+
+    //popup
+    const togglePopUp = () => {
+        const popup = document.querySelector('.popup'),
+        popupBtn = document.querySelectorAll('.popup-btn'),
+        popupClose = document.querySelector('.popup-close');
+        const showAnimation = () => {
+            const popupAnimation = popup.animate([
+              { opacity: 0 },
+              { opacity: 1 },
+            ], 700);
+            popupAnimation.removeEventListener('finish', () => {
+              popup.style.opacity = 1;
+            });
+            popupAnimation.addEventListener('finish', () => {
+              popup.style.opacity = 1;
+            });
+          };
+
+        popupBtn.forEach((elem)  => {
+            elem.addEventListener('click', () => {
+                if (document.documentElement.offsetWidth >= 768) showAnimation();
+                popup.style.display = 'block';
+            });
+        });
+        popupClose.addEventListener('click', () => popup.style.display = 'none');
+        
+    };
+
+    togglePopUp();
+
+
+    
+
+
+
 
 
 
