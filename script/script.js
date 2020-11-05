7d8b2661920e7a092db736f421846c972e15ff28
Alexander <57403744+xSeftx@users.noreply.github.com>
2020-11-05 20:52:46 +0400

    Добавил задание №21

--- script/script.js
+++ script/script.js
@@ -141,6 +141,118 @@ window.addEventListener('DOMContentLoaded', function(){
                 });
             }
         });
+
+        //слайдер
+        const slider = () => {
+
+            const slide = document.querySelectorAll('.portfolio-item'),
+                btn = document.querySelectorAll('.potfolio-btn'),
+                
+                slider = document.querySelector('.portfolio-content'),
+                dots = document.querySelector('.portfolio-dots');
+            let currentSlide = 0,
+                interval;
+            
+            const addDots = () => {        
+                        
+                for(let i = 0; i<slide.length; i++){
+                    let li = document.createElement('li');
+                    if (i===0){                    
+                        li.classList.add('dot', 'dot-active');
+                        
+                    }else {                        
+                        li.classList.add('dot');                        
+                    }
+                    dots.append(li);                   
+                }                    
+            }; 
+            addDots();            
+            const dot = document.querySelectorAll('.dot');
+            
+            const prevSlide = (elem, index, strClass) => {
+                elem[index].classList.remove(strClass);
+            };
+
+            const nextSlide = (elem, index, strClass) => {
+                elem[index].classList.add(strClass);
+            };
+
+            const autoPlaySlide = () => {
+
+                prevSlide(slide, currentSlide, 'portfolio-item-active');
+                prevSlide(dot, currentSlide, 'dot-active');
+                currentSlide++;
+                if(currentSlide >= slide.length){
+                    currentSlide = 0;
+                }
+                nextSlide(slide, currentSlide, 'portfolio-item-active');
+                nextSlide(dot, currentSlide, 'dot-active');
+            };
+
+            const startSlide = (time=3000) => {
+               interval = setInterval(autoPlaySlide, time);
+            };
+
+            const stopSlide = () => {
+                clearInterval(interval);
+            };
+
+            slider.addEventListener('click', (event) => {
+                event.preventDefault();
+
+                let target = event.target;
+
+                if(!target.matches('.portfolio-btn, .dot')){
+                    return;
+                }
+
+                prevSlide(slide, currentSlide, 'portfolio-item-active');
+                prevSlide(dot, currentSlide, 'dot-active');
+
+                if(target.matches('#arrow-right')) {
+                    currentSlide++;
+                }else if(target.matches('#arrow-left')) {
+                    currentSlide--;
+                }else if (target.matches('.dot')) {
+                    dot.forEach((elem, index) => {
+                        if (elem === target){
+                            currentSlide = index;
+                        }
+                    });
+                }
+
+                if(currentSlide >= slide.length){
+                    currentSlide = 0;
+                }
+
+                if(currentSlide < 0){
+                    currentSlide = slide.length-1;
+                }
+
+                nextSlide(slide, currentSlide, 'portfolio-item-active');
+                nextSlide(dot, currentSlide, 'dot-active');
+
+            });
+
+            slider.addEventListener('mouseover', (event) => {
+                if(event.target.matches('.portfolio-btn') || event.target.matches('.dot')){
+                    stopSlide();
+                }
+            });
+
+            slider.addEventListener('mouseout', (event) => {
+                if(event.target.matches('.portfolio-btn') || event.target.matches('.dot')){
+                    startSlide();
+                }
+            });
+            
+            startSlide(1000000);
+
+        };
+
+        slider();
+
+
     };
 
     tabs();
