d28028c3d19474d9596420b5ea402d941b12a0e1
Alexander <57403744+xSeftx@users.noreply.github.com>
2020-11-10 18:29:49 +0400

    Add lesson24

--- script/script.js
+++ script/script.js
@@ -274,6 +274,53 @@ window.addEventListener('DOMContentLoaded', function(){
     dataImg();
 
     // калькулятор
+    const calc = (price = 100) => {
+        const calcBlock = document.querySelector('.calc-block'),
+            calcType = document.querySelector('.calc-type'),
+            calcSquare = document.querySelector('.calc-square'),
+            calcDay = document.querySelector('.calc-day'),
+            calcCount = document.querySelector('.calc-count'),
+            totalValue = document.getElementById('total');
+
+        const countSum = () => {
+            let total = 0,
+                countValue = 1,
+                dayValue = 1;                
+            const squareValue = +calcSquare.value,
+                typeValue = calcType.options[calcType.selectedIndex].value;
+
+            if(calcCount.value > 1){
+                countValue += (calcCount.value - 1) / 10;
+            }
+
+            if(calcDay.value && calcDay.value < 5){
+                dayValue *= 2;
+            }else if(calcDay.value && calcDay.value < 10){
+                dayValue *= 1.5;
+            }
+
+
+            if(typeValue && squareValue){
+                total = price * typeValue * squareValue * countValue * dayValue;
+            }
+
+
+            totalValue.textContent = total;
+        };
+        
+        calcBlock.addEventListener('change', (event) => {
+            const target = event.target;
+            if (target.matches('.calc-type, .calc-square, .calc-day, .calc-count')) {
+                countSum();
+            }
+
+        });
+
+    };
+
+    calc();
+
+
     const inputCalculator = () => {
         const inputCalc = document.querySelectorAll('.calc-block input[type=text]');
         inputCalc.forEach((item) => {
