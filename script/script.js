d014d24a902180474166615116c81045e5e158f6
Alexander <57403744+xSeftx@users.noreply.github.com>
2020-11-10 21:06:58 +0400

    add lesson24-1

--- script/script.js
+++ script/script.js
@@ -280,7 +280,7 @@ window.addEventListener('DOMContentLoaded', function(){
             calcSquare = document.querySelector('.calc-square'),
             calcDay = document.querySelector('.calc-day'),
             calcCount = document.querySelector('.calc-count'),
-            totalValue = document.getElementById('total');
+            totalValue = document.getElementById('total');        
 
         const countSum = () => {
             let total = 0,
@@ -303,10 +303,25 @@ window.addEventListener('DOMContentLoaded', function(){
             if(typeValue && squareValue){
                 total = price * typeValue * squareValue * countValue * dayValue;
             }
+            
 
+            if(total){
+                const interval = setInterval(function(){
+                    if(totalValue.innerHTML*1+50 <= total){
+                        totalValue.innerHTML= totalValue.
+                        innerHTML*1+50;
+                    } else if(totalValue.innerHTML*1-50 >= total){
+                        totalValue.innerHTML = totalValue.innerHTML*1-50;
+                    }
+                    else{
+                        clearInterval(interval);
+                    }   
+                });
+            
 
-            totalValue.textContent = total;
-        };
+            }
+            
+        };        
         
         calcBlock.addEventListener('change', (event) => {
             const target = event.target;
